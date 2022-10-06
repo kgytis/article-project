5d30727f0feafbf2e97329e5d259f60314a9f860
kgytis <kaziukon.gytis@gmail.com>
2022-10-06 13:05:23 +0300

    404 page added

---
+++ src/layouts/PageNotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <center>
+    <div>
+      <h1 class="is-size-2">404 Page not Found!</h1>
+      <p class="is-size-2">
+        <a href="/">Go home?</a>
+      </p>
+    </div>
+  </center>
+</template>
+
+<style scoped>
+center {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center; 
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Router from 'vue-router';
 // Page component imports
 import Articles from './layouts/ArticlesPage.vue';
 import Article from './layouts/ArticlePage.vue';
+import PageNotFound from './layouts/PageNotFound.vue'
 
 Vue.use(Router);
 const router = new Router({
@@ -11,6 +12,7 @@ const router = new Router({
     { path: '/', component: Articles },
     { path: '/articles', component: Articles },
     { path: '/articles/:id', component: Article, props: true },
+    {path : "*", component: PageNotFound}
   ],
 });
 export default router;
