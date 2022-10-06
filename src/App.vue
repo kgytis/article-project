425ae2d1c70185b395089a929fb048d1853bd0d8
kgytis <kaziukon.gytis@gmail.com>
2022-10-06 09:35:58 +0300

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,3 @@
   </div>
 </template>
 
-<script>
-
-export default {
-// name: 'app',
-  // data () {
-  //   return {
-  //     msg: 'Welcome to Your Vue.js App'
-  //   }
-  // }
-}
-</script>
-
-<style>
-
-
-</style>
